60382fdc8065effc15c5fd28c30f18acf554bd00
Hasan Naseem <hn92@live.com>
2018-04-12 19:51:25 -0400

    Add show page and new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'entries/index'
+
+  get 'entries/new'
+
+  get 'entries/edit'
+
+  get 'entries/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :entries
+
 end
