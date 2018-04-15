3a411db0f6d05aee5ae1eb5d050a34dcd918ac03
Hasan Naseem <hn92@live.com>
2018-04-15 19:51:55 -0400

    add new word

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,18 +1,30 @@
 class EntriesController < ApplicationController
   def index
+    @entries = Entry.all
   end
 
   def new
+    @entry = Entry.new
   end
 
   def edit
   end
 
   def show
+    @entry = Entry.find(params[:id])
   end
 
   def create
-    redirect_to entries_url
+    @entry = Entry.new
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entries_url
+    else
+      redirect_to entries_url
+    end
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
-  get 'entries/index'
-
-  get 'entries/new'
-
-  get 'entries/edit'
-
-  get 'entries/show'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'entries#index'
   resources :entries
 
 end
