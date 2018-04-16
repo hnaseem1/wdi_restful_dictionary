f3a4b7dc54b3508c88fe28c54cc970f7d5429dc0
Hasan Naseem <hn92@live.com>
2018-04-15 20:37:09 -0400

    Edit not working properly

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,10 +8,26 @@ class EntriesController < ApplicationController
   end
 
   def edit
+    @entry = Entry.find(params[:id])
   end
 
   def show
     @entry = Entry.find(params[:id])
+  end
+
+  def update
+
+    @entry = Entry.new
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entries_url(@entry)
+    else
+      redirect_to entries_url(@entry)
+    end
+
   end
 
   def create
@@ -23,13 +39,15 @@ class EntriesController < ApplicationController
     if @entry.save
       redirect_to entries_url
     else
-      redirect_to entries_url
+      redirect_to new_entries_url
     end
   end
 
-  def edit
-    redirect_to entry_url(params[:id])
-  end
+
+
+  # def edit
+  #   # redirect_to entry_url(params[:id])
+  # end
 
   def destroy
     redirect_to entries_url
